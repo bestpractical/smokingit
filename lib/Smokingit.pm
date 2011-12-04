72447ea022911f3498e725f860423d5689c9a7df
Alex Vandiver <alex@chmrr.net>
2011-12-04 15:34:35 -0500

    Add memcached connection, for caching test statuses

--- lib/Smokingit.pm
+++ lib/Smokingit.pm
@@ -3,14 +3,18 @@ use warnings;
 
 package Smokingit;
 use Gearman::Client;
+use Cache::Memcached;
 
-our $GEARMAN;
+our( $GEARMAN, $MEMCACHED );
 
 sub start {
     $GEARMAN = Gearman::Client->new;
     $GEARMAN->job_servers( Jifty->config->app('job_servers') );
+
+    $MEMCACHED = Cache::Memcached->new(
+        { servers => Jifty->config->app( 'memcached_servers' ) } );
 }
 
-sub gearman { $GEARMAN }
-
+sub gearman   { $GEARMAN   }
+sub memcached { $MEMCACHED }
 1;
