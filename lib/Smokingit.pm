6f601d78ae1efeeeb87a38b0534b735bc85c185b
Alex Vandiver <alexmv@bestpractical.com>
2012-03-12 23:53:31 -0400

    Factor out check_queue into Smokingit.pm

--- lib/Smokingit.pm
+++ lib/Smokingit.pm
@@ -17,4 +17,21 @@ sub start {
 
 sub gearman   { $GEARMAN   }
 sub memcached { $MEMCACHED }
+
+sub check_queue {
+    my $job = shift;
+    my $queued = Smokingit::Model::SmokeResultCollection->new;
+    $queued->limit(
+        column => "submitted_at",
+        operator => "IS",
+        value => "NULL",
+    );
+    my $restarted = 0;
+    while (my $smoke = $queued->next) {
+        next if $smoke->gearman_status->known;
+        $restarted += $smoke->run_smoke;
+    }
+    return $restarted;
+}
+
 1;
