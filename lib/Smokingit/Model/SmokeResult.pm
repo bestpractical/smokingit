546000e48ce504250562ec41a19a7083b294a5d8
Alex Vandiver <alexmv@bestpractical.com>
2012-03-12 23:59:03 -0400

    Factor out smoke submission into a method

--- lib/Smokingit/Model/SmokeResult.pm
+++ lib/Smokingit/Model/SmokeResult.pm
@@ -4,7 +4,7 @@ use warnings;
 package Smokingit::Model::SmokeResult;
 use Jifty::DBI::Schema;
 
-use Storable qw/nfreeze/;
+use Storable qw/nfreeze thaw/;
 
 use Smokingit::Record schema {
     column project_id =>
@@ -128,6 +128,69 @@ sub run_smoke {
 sub status_cache_key {
     my $self = shift;
     return "status-" . $self->commit->sha . "-" . $self->configuration->id;
+}
+
+sub post_result {
+    my $self = shift;
+    my ($arg) = @_;
+
+    my %result;
+    eval { %result = %{ thaw( $arg ) } };
+    return (0, "Thaw failed: $@") if $@;
+
+    # Properties to extract from the aggregator
+    my @props =
+        qw/failed
+           parse_errors
+           passed
+           planned
+           skipped
+           todo
+           todo_passed
+           wait
+           exit/;
+
+    # Aggregator might not exist if we had a configure failure
+    require TAP::Parser::Aggregator;
+    my $a = $result{aggregator};
+    if ($a) {
+        $result{$_} = $a->$_ for @props;
+        $result{is_ok}      = not($a->has_problems);
+        $result{elapsed}    = $a->elapsed->[0];
+        $result{error}      = undef;
+    } else {
+        # Unset the existing data if there was a fail
+        $result{$_} = undef for @props, "is_ok", "elapsed";
+    }
+    $result{submitted_at} = Jifty::DateTime->now;
+
+    # Find the smoke
+    Jifty->handle->begin_transaction;
+    my $smokeid = delete $result{smoke_id};
+    $self->load( $smokeid );
+    if (not $self->id) {
+        return (0, "Invalid smoke ID: $smokeid");
+    } elsif (not $self->gearman_process) {
+        return (0, "Smoke report on $smokeid which wasn't being smoked? (last report at @{[$self->submitted_at]})");
+    }
+
+    # Update with the new data
+    for my $key (keys %result) {
+        my $method = "set_$key";
+        $self->$method($result{$key});
+    }
+    # Mark as no longer smoking
+    $self->set_gearman_process(undef);
+
+    # And commit all of that
+    Jifty->handle->commit;
+
+    return (1, "Test result for "
+             . $self->project->name
+             ." ". $self->commit->short_sha
+             ." using ". $self->configuration->name
+             ." on ". $self->branch_name
+             .": ".($self->is_ok ? "OK" : "NOT OK"));
 }
 
 1;
