0088b3e98644f1154f326367295010e7f9679b7d
Alex Vandiver <alexmv@bestpractical.com>
2012-03-23 04:31:48 -0400

    Login and logout

--- lib/Smokingit/View/Page.pm
+++ lib/Smokingit/View/Page.pm
@@ -9,6 +9,58 @@ use Jifty::View::Declare::Helpers;
 sub render_page {
     my $self = shift;
     my $title = shift;
+
+    div {
+        { id is 'authbox' };
+        if (Jifty->web->current_user->id) {
+            div {
+                { class is 'username' };
+                js_handlers {
+                    onclick => "jQuery('#authbox .logout').toggle()" };
+                outs(Jifty->web->current_user->username);
+                div {
+                    { class is 'logout' };
+                    form {
+                        my $logout = new_action(
+                            class => "Logout",
+                            moniker => "logout"
+                        );
+                        form_submit( submit => $logout, label => "Log out" );
+                    }
+                }
+            }
+        } else {
+            div {
+                { class is 'prompt' };
+                js_handlers {
+                    onclick => [
+                        "jQuery('#authbox .prompt').hide()",
+                        "jQuery('#authbox .loginform').show()",
+                        "jQuery('.loginform input.argument-username').focus()",
+                    ],
+                };
+                "Login";
+            };
+            div {
+                { class is 'loginform' };
+                form {
+                    my $login = new_action(
+                        class => "Login",
+                        moniker => "login",
+                    );
+                    render_param( $login => $_ )
+                        for qw/username password token hashed_password/;
+                    render_param(
+                        $login => "remember",
+                        render_as => 'hidden',
+                        default_value => 1
+                    );
+                    form_submit( label => "Login", onclick => "return getPasswordToken('login');" );
+                };
+            }
+        }
+    }
+
     div {
         { id is 'content' };
         $self->instrument_content;
