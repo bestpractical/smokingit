7b905dae939d95e963b135508a2c921bbee07638
Alex Vandiver <alexmv@bestpractical.com>
2012-06-20 02:02:30 -0400

    Refactor testing into an action

--- lib/Smokingit.pm
+++ lib/Smokingit.pm
@@ -13,4 +13,19 @@ sub start {
 
 sub memcached { $MEMCACHED }
 
+sub test {
+    my $self = shift;
+
+    my $arg = shift;
+    my $action = Smokingit::Action::Test->new(
+        current_user => Smokingit::CurrentUser->superuser,
+        arguments    => { commit => $arg },
+    );
+    $action->validate;
+    return $action->result->field_error("commit") . "\n"
+        unless $action->result->success;
+    $action->run;
+    return ($action->result->message || $action->result->error);
+}
+
 1;
---
+++ lib/Smokingit/Action/Test.pm
@@ -0,0 +1,168 @@
+use strict;
+use warnings;
+
+=head1 NAME
+
+Smokingit::Action::Test
+
+=cut
+
+package Smokingit::Action::Test;
+use base qw/Smokingit::Action/;
+
+use Jifty::Param::Schema;
+use Jifty::Action schema {
+    param 'commit' =>
+        label is 'Commit to test',
+        is mandatory;
+};
+
+sub validate_commit {
+    my $self = shift;
+    my $arg = shift;
+
+    unless ($arg =~ /^([0-9a-fA-F]+)(?:\s*\[\s*(.*)\s*\])?(?:\s*\{\s*(.*)\s*\})?$/) {
+        return $self->validation_error(
+            commit => "That doesn't look like a SHA" );
+    }
+    my ($sha,$config_text, $branch_text) = ($1, $2, $3);
+
+    my $commits = Smokingit::Model::CommitCollection->new;
+    $commits->limit( column => "sha", operator => "like", value => "$sha%" );
+    return $self->validation_error(
+        commit => "Unknown SHA $sha"
+    ) if $commits->count == 0;
+    return $self->validation_error(
+        commit => "Ambiguous SHA (".$commits->count." matches)"
+    ) if $commits->count > 1;
+
+    my $commit = $commits->first;
+    $sha = $commit->sha;
+
+    if (not $config_text) {
+        # We'll do all of them
+    } elsif ($config_text =~ /^\d+$/) {
+        my $config = Smokingit::Model::Configuration->new;
+        $config->load( $config_text );
+        return $self->validation_error( commit => "Invalid configuration id" )
+            unless $config->id and $config->project->id == $commit->project->id;
+    } else {
+        my $configs = $commit->project->configurations;
+        $configs->limit( column => "name", operator => "MATCHES", value => $config_text );
+        return $self->validation_error( commit => "Invalid configuration name" )
+            unless $configs->count == 1;
+        $config_text = $configs->first->id;
+    }
+
+    my $existing = Smokingit::Model::SmokeResultCollection->new;
+    $existing->limit(
+        column => "project_id",
+        value  => $commit->project->id
+    );
+    $existing->limit(
+        column => "commit_id",
+        value => $commit->id
+    );
+    $existing->limit(
+        column => "configuration_id",
+        value => $config_text
+    ) if $config_text;
+
+    if ($existing->count) {
+        # Re-testing the existing smokes
+        undef $branch_text;
+    } else {
+        my @branches = $commit->branches;
+        if ($branch_text) {
+            if ($branch_text =~ /^\d+$/) {
+                my $branch = Smokingit::Model::Branch->new;
+                $branch->load($branch_text);
+                return $self->validation_error( commit => "Invalid branch id" )
+                    unless $branch->id;
+                $branch_text = $branch->name;
+            }
+            return $self->validation_error( commit => "Invalid branch name" )
+                unless grep {$_ eq $branch_text} @branches;
+        } elsif (@branches == 1) {
+            $branch_text = $branches[0];
+        } else {
+            return $self->validation_error( commit => "Can't determine which branch to test on" );
+        }
+    }
+
+    $sha .= "[$config_text]" if $config_text;
+    $sha .= "{$branch_text}" if $branch_text;
+    $self->argument_value( commit => $sha );
+    return $self->validation_ok( "commit" );
+}
+
+sub take_action {
+    my $self = shift;
+
+    my $arg = $self->argument_value("commit");
+    my ($sha, $config, $branchname) =
+        $arg =~ /^([0-9a-fA-F]+)(?:\[(\d+)\])?(?:\{(.*)\})?$/;
+
+    my $commit = Smokingit::Model::Commit->new;
+    $commit->load_by_cols( sha => $sha );
+
+    if ($branchname) {
+        # Testing new commits
+        my $branch = Smokingit::Model::Branch->new;
+        $branch->load_by_cols( name => $branchname, project_id => $commit->project->id);
+
+        my $configs = $commit->project->configurations;
+        $configs->limit( column => "id", value => $config ) if $config;
+
+        while (my $config = $configs->next) {
+            $commit->run_smoke( $config, $branch );
+        }
+        $self->result->message(
+            "Testing "
+              . $configs->count
+              . " configurations of "
+              . $commit->short_sha
+          );
+    } else {
+        # Re-testing old commits
+        my $existing = Smokingit::Model::SmokeResultCollection->new;
+        $existing->limit(
+            column => "project_id",
+            value  => $commit->project->id,
+        );
+        $existing->limit(
+            column => "commit_id",
+            value => $commit->id,
+        );
+        $existing->limit(
+            column => "queue_status",
+            operator => "IS",
+            value => "NULL",
+        );
+        $existing->limit(
+            column => "configuration_id",
+            value => $config,
+        ) if $config;
+        $existing->order_by( column => 'queued_at' );
+
+        while (my $smoke = $existing->next) {
+            $smoke->as_superuser->set_submitted_at(undef);
+            $smoke->as_superuser->set_queue_status(undef);
+            $smoke->run_smoke;
+        }
+
+        if ($existing->count == 0) {
+            $self->result->message("Commits already queued for testing");
+        } else {
+            $self->result->message(
+                "Retesting "
+                  . $existing->count
+                  . " configurations of "
+                  . $commit->short_sha
+              );
+        }
+
+    }
+}
+
+1;
