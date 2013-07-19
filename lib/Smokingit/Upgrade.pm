4cfca5e2cecfd31c051ddb68adedd9f834755e4f
Alex Vandiver <alexmv@bestpractical.com>
2013-07-19 01:43:45 -0400

    Add an upgrade step to build SmokeFileResults out of aggregators

--- lib/Smokingit/Upgrade.pm
+++ lib/Smokingit/Upgrade.pm
@@ -34,9 +34,46 @@ since '0.0.4' => sub {
     }
 };
 
-# Time elapsed became a float
 since '0.0.8' => sub {
+    # Time elapsed became a float
     Jifty->handle->simple_query("ALTER TABLE smoke_results ALTER COLUMN elapsed TYPE float");
+
+    # Go through and inflate all of the old aggregators into test result rows
+    my $tests = Smokingit::Model::SmokeResultCollection->new( current_user => $super );
+    $tests->unlimit;
+    $tests->columns( "id", "aggregator" );
+    require TAP::Parser;
+    require TAP::Parser::Aggregator;
+    while (my $test = $tests->next) {
+        my $aggregator = $test->_value('aggregator');
+        for my $filename ($aggregator->descriptions) {
+            my ($parser) = $aggregator->parsers($filename);
+
+            my $tap = "";
+            if ($parser->skip_all) {
+                $tap = "1..0 # skipped\n";
+            } else {
+                $tap = $parser->plan . "\n";
+                my %lines;
+                $lines{$_}   = "ok $_ # skip" for $parser->skipped;
+                $lines{$_} ||= "ok $_ # TODO" for $parser->todo_passed;
+                $lines{$_} ||= "not ok $_ # TODO" for $parser->todo;
+                $lines{$_} ||= "ok $_" for $parser->actual_passed;
+                $lines{$_} ||= "not ok $_" for $parser->actual_failed;
+                $tap .= "$lines{$_}\n" for sort {$a <=> $b} keys %lines;
+            }
+
+            my $filetest = Smokingit::Model::SmokeFileResult->new( current_user => $super );
+            $filetest->create(
+                smoke_result_id => $test->id,
+                filename        => $filename,
+                elapsed         => ($parser->end_time - $parser->start_time),
+                is_ok           => !$parser->has_problems,
+                tests_run       => $parser->tests_run,
+                raw_tap         => $tap,
+            );
+        }
+    }
 };
 
 1;
