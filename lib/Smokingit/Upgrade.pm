2836a8bc3053ca32faf1ef0d05780ba79e4b6e86
Alex Vandiver <alexmv@bestpractical.com>
2013-07-22 02:32:12 -0400

    Switch to batching the upgrade, soas to not attempt to fit everything in memory

--- lib/Smokingit/Upgrade.pm
+++ lib/Smokingit/Upgrade.pm
@@ -38,42 +38,53 @@ since '0.0.8' => sub {
     # Time elapsed became a float
     Jifty->handle->simple_query("ALTER TABLE smoke_results ALTER COLUMN elapsed TYPE float");
 
-    # Go through and inflate all of the old aggregators into test result rows
-    my $tests = Smokingit::Model::SmokeResultCollection->new( current_user => $super );
-    $tests->unlimit;
-    $tests->columns( "id", "aggregator" );
     require TAP::Parser;
     require TAP::Parser::Aggregator;
-    while (my $test = $tests->next) {
-        my $aggregator = $test->_value('aggregator');
-        for my $filename ($aggregator->descriptions) {
-            my ($parser) = $aggregator->parsers($filename);
 
-            my $tap = "";
-            if ($parser->skip_all) {
-                $tap = "1..0 # skipped\n";
-            } else {
-                $tap = $parser->plan . "\n";
-                my %lines;
-                $lines{$_}   = "ok $_ # skip" for $parser->skipped;
-                $lines{$_} ||= "ok $_ # TODO" for $parser->todo_passed;
-                $lines{$_} ||= "not ok $_ # TODO" for $parser->todo;
-                $lines{$_} ||= "ok $_" for $parser->actual_passed;
-                $lines{$_} ||= "not ok $_" for $parser->actual_failed;
-                $tap .= "$lines{$_}\n" for sort {$a <=> $b} keys %lines;
+    # Go through and inflate all of the old aggregators into test result
+    # rows; do this in batches of 100, to save on memory.
+    my $max = 0;
+    do {
+        my $tests = Smokingit::Model::SmokeResultCollection->new( current_user => $super );
+        $tests->limit( column => "id", operator => ">", value => $max );
+        $tests->order_by( { column => "id", order  => "asc" } );
+        $tests->rows_per_page(100);
+        $tests->columns( "id", "aggregator" );
+        $max = 0;
+        while (my $test = $tests->next) {
+            $max = $test->id;
+            warn "$max\n";
+            my $aggregator = $test->_value('aggregator');
+            next unless $aggregator;
+            for my $filename ($aggregator->descriptions) {
+                my ($parser) = $aggregator->parsers($filename);
+
+                my $tap = "";
+                if ($parser->skip_all) {
+                    $tap = "1..0 # skipped\n";
+                } else {
+                    $tap = $parser->plan . "\n";
+                    my %lines;
+                    $lines{$_}   = "ok $_ # skip" for $parser->skipped;
+                    $lines{$_} ||= "ok $_ # TODO" for $parser->todo_passed;
+                    $lines{$_} ||= "not ok $_ # TODO" for $parser->todo;
+                    $lines{$_} ||= "ok $_" for $parser->actual_passed;
+                    $lines{$_} ||= "not ok $_" for $parser->actual_failed;
+                    $tap .= "$lines{$_}\n" for sort {$a <=> $b} keys %lines;
+                }
+
+                my $filetest = Smokingit::Model::SmokeFileResult->new( current_user => $super );
+                $filetest->create(
+                    smoke_result_id => $test->id,
+                    filename        => $filename,
+                    elapsed         => ($parser->end_time - $parser->start_time),
+                    is_ok           => !$parser->has_problems,
+                    tests_run       => $parser->tests_run,
+                    raw_tap         => $tap,
+                );
             }
-
-            my $filetest = Smokingit::Model::SmokeFileResult->new( current_user => $super );
-            $filetest->create(
-                smoke_result_id => $test->id,
-                filename        => $filename,
-                elapsed         => ($parser->end_time - $parser->start_time),
-                is_ok           => !$parser->has_problems,
-                tests_run       => $parser->tests_run,
-                raw_tap         => $tap,
-            );
         }
-    }
+    } while ($max);
 };
 
 1;
